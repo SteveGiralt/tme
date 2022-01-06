dc68899e376bffe62c3e7ec0c8b5031590ad3f66
Steve Giralt <steve.giralt@gmail.com>
2022-01-05 23:07:13 -0700

    initial commit - It's ALIVE!

---
+++ runner.js
@@ -0,0 +1,56 @@
+const fs = require("fs");
+const path = require("path");
+const chalk = require("chalk");
+
+const forbiddenDirs = ["node_modules"];
+
+class Runner {
+  constructor() {
+    this.testFiles = [];
+  }
+
+  async runTests() {
+    for (let file of this.testFiles) {
+      console.log(chalk.gray(`---=> ${file.shortName}`));
+      const beforeEachs = [];
+      global.beforeEach = (fn) => {
+        beforeEachs.push(fn);
+      };
+      global.it = (desc, fn) => {
+        beforeEachs.forEach((func) => func());
+        try {
+          fn();
+          console.log(chalk.blue(`\tOK - ${desc}`));
+        } catch (err) {
+          const message = err.message.replace(/\n/g, "\n\t\t");
+          console.log(chalk.red(`\tFAIL - ${desc}`));
+          console.log(chalk.red("\t", message));
+        }
+      };
+      try {
+        require(file.name);
+      } catch (err) {
+        console.log(chalk.red(err));
+      }
+    }
+  }
+
+  collectFiles() {}
+  async collectFiles(targetPath) {
+    const files = await fs.promises.readdir(targetPath);
+    for (let file of files) {
+      const filepath = path.join(targetPath, file);
+      const stats = await fs.promises.lstat(filepath);
+
+      if (stats.isFile() && file.includes(".test.js")) {
+        this.testFiles.push({ name: filepath, shortName: file });
+      } else if (stats.isDirectory() && !forbiddenDirs.includes(file)) {
+        const childFiles = await fs.promises.readdir(filepath);
+
+        files.push(...childFiles.map((f) => path.join(file, f)));
+      }
+    }
+  }
+}
+
+module.exports = Runner;
